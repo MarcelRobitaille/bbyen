9bf98289fde795a586cb8305cf9064e747b07944
Marcel Robitaille <mail@marcelrobitaille.me>
2020-08-24 13:17:59 -0300

    Wrap processing of each video in try / catch

--- src/videos.js
+++ src/videos.js
@@ -42,56 +42,61 @@ const parseFeedsAndNotify = async ({
 			`.trim())
 
 			for (let { videoId } of feed.items) {
+				try {
 
-				if (videosSent.has(videoId)) continue
+					if (videosSent.has(videoId)) continue
 
-				const details = (await service.videos.list({
-					auth,
-					part: 'contentDetails,snippet',
-					id: videoId,
-				})).data.items[0]
+					const details = (await service.videos.list({
+						auth,
+						part: 'contentDetails,snippet',
+						id: videoId,
+					})).data.items[0]
 
-				const { title: videoTitle, channelTitle: channelName } =
-					details.snippet.title
-				const videoThumbnail = details.snippet.thumbnails.high.url
-				const videoDuration = details.contentDetails.duration
-					.replace('PT', '')
-					.replace('S', '')
-					.replace('M', ':')
-					.replace('H', ':')
+					const { title: videoTitle, channelTitle: channelName } =
+						details.snippet.title
+					const videoThumbnail = details.snippet.thumbnails.high.url
+					const videoDuration = details.contentDetails.duration
+						.replace('PT', '')
+						.replace('S', '')
+						.replace('M', ':')
+						.replace('H', ':')
 
-				console.log(
-					chalk.magenta('[videos]'),
-					`New video (${videoId}): ${videoTitle}`,
-				)
+					console.log(
+						chalk.magenta('[videos]'),
+						`New video (${videoId}): ${videoTitle}`,
+					)
 
-				await db.run(SQL`
-					INSERT INTO videos (videoId, channelId)
-					VALUES (${videoId}, ${channelId});
-				`)
+					await db.run(SQL`
+						INSERT INTO videos (videoId, channelId)
+						VALUES (${videoId}, ${channelId});
+					`)
 
-				const info = await transporter.sendMail({
-					from: config.email.sendingContact,
-					to: config.email.destination,
-					subject: `${channelName} just uploaded a video`,
-					html: emailTemplate({
-						channelId,
-						channelName,
-						channelThumbnail,
-						videoId,
-						videoThumbnail,
-						videoTitle,
-						videoDuration,
-						videoURL: [
-							'https://www.youtube.com/attribution_link?u=/',
-							encodeURIComponent(`watch?v=${videoId}`),
-						].join(''),
-					}),
-				})
+					const info = await transporter.sendMail({
+						from: config.email.sendingContact,
+						to: config.email.destination,
+						subject: `${channelName} just uploaded a video`,
+						html: emailTemplate({
+							channelId,
+							channelName,
+							channelThumbnail,
+							videoId,
+							videoThumbnail,
+							videoTitle,
+							videoDuration,
+							videoURL: [
+								'https://www.youtube.com/attribution_link?u=/',
+								encodeURIComponent(`watch?v=${videoId}`),
+							].join(''),
+						}),
+					})
 
-				console.log(info)
+					console.log(info)
 
-				break
+					break
+
+				} catch (err) {
+					console.error(err)
+				}
 			}
 		}
 	} catch (err) {
