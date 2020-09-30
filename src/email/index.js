0b3797024750644611bde8e00bd7101c6b08dae8
Marcel Robitaille <mail@marcelrobitaille.me>
2020-09-30 09:36:39 -0400

    Switch to ejs
    I want my ternary operator

--- src/email/index.js
+++ src/email/index.js
@@ -1,14 +1,14 @@
 const fs = require('pn/fs')
 const path = require('path')
 
+const ejs = require('ejs')
 const nodemailer = require('nodemailer')
-const Handlebars = require('handlebars')
 
 
 const init = async (config) => {
 	const transporter = nodemailer.createTransport(config)
-	const emailTemplate = Handlebars.compile(await fs.readFile(
-		path.join(__dirname, './template.hbs'),
+	const emailTemplate = ejs.compile(await fs.readFile(
+		path.join(__dirname, './template.ejs'),
 		'utf-8',
 	))
 
